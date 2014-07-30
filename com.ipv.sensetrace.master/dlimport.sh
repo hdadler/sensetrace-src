94fb7a967c11819808503e06ef72edb79b56f184
hdadler <hdadler@users.noreply.github.com>
2014-07-30 18:43:48 +0200

    added path to osgi env in dlimport.sh script

--- com.ipv.sensetrace.master/dlimport.sh
+++ com.ipv.sensetrace.master/dlimport.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
+#Please set path to org.eclipse.osgi...jar
+
 export LD_LIBRARY_PATH=/usr/local/lib/swipl-6.6.1/lib/x86_64-linux:/usr/local/lib/swipl-6.6.1/bin/x86_64-linux
 
-java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dimport_from_dl=true -jar  org.eclipse.osgi_3.9.1.v20140110-1610.jar os linux -ws gtk -arch x86_64 -consoleLog -console
+java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dimport_from_dl=true -jar   /home/sensetrace/export/org.eclipse.osgi_3.9.1.v20140110-1610.jar os linux -ws gtk -arch x86_64 -consoleLog -console
+
